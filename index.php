0e9f46bdfa4b03f3f2be6b15247f559168225484
alexparavi <alexparavi@gmail.com>
2026-06-01 16:24:51 +0400

    chore: add test execution

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
+
+use Apara\PhpArchitectureLearning\Formatters\LowerCaseFormatter;
+use Apara\PhpArchitectureLearning\Formatters\UpperCaseFormatter;
+use Apara\PhpArchitectureLearning\TextProcessor;
+
+$strategy1 = new UpperCaseFormatter;
+$strategy2 = new LowerCaseFormatter;
+$process = new TextProcessor($strategy1);
+
+$text = 'helLo world';
+
+$result1 = $process->process($text);
+$process = new TextProcessor($strategy2);
+$result2 = $process->process($text);
+
+var_dump($result2);
