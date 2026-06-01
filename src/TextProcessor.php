8eeec90c86b7d5f33e10e69b79ad8548fb052754
alexparavi <alexparavi@gmail.com>
2026-06-01 16:12:45 +0400

    feat: create TextProcessor context class

---
+++ src/TextProcessor.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Apara\PhpArchitectureLearning;
+
+use Apara\PhpArchitectureLearning\Formatters\FormatterInterface;
+
+class TextProcessor
+{
+    private FormatterInterface $formatter;
+    public function __construct(FormatterInterface $formatter)
+    {
+        $this->formatter = $formatter;
+    }
+
+    public function process(string $text): string
+    {
+        $result = $this->formatter->format($text);
+        return $result;
+    }
+}
